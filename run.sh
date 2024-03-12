c26227e15a01c50e6d3c7c3683457d2d8e3325af
gabrieldernbach <gabriel.dernbach@gmail.com>
2024-03-12 16:16:56 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+python -m torch.distributed.launch \
+        --nnodes=1 --node_rank=0 \
+        --nproc_per_node=1 \
+        --master_addr="127.0.0.1" \
+        --master_port=29501 \
+        classification/main.py --cfg classification/configs/vssm1/vssm_tiny_224_0229.yaml \
+                --batch-size 128 \
+                --data-path dataset \
+                --output /tmp
