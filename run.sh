a47ed815ad7626610fc9359c81bf248628e18140
gabrieldernbach <gabriel.dernbach@gmail.com>
2024-03-12 18:32:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python -m torch.distributed.launch \
-        --nnodes=1 --node_rank=0 \
+        --nnodes=1 \
+        --node_rank=0 \ 
         --nproc_per_node=1 \
         --master_addr="127.0.0.1" \
         --master_port=29501 \
